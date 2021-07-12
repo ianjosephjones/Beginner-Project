67ac51176fcb7b08a766083b1c49938d6f7ce4bc
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-12 15:12:33 -0400

    fixed

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+// const $ = (selector) => {
+//     console.log(selector);    
+//     return document.querySelector(selector) 
+// };
+
+// const menu = $('#mobile-menu')
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
-menu.addEventListener('click', function() {
+menu.addEventListener('click', toggleMenuActive);
+
+function toggleMenuActive() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-})
+}
